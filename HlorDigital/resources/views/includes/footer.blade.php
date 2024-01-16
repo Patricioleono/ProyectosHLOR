7ff20c845cb01ac619e29628e2dc4e6c33e66627
Patricio Leon Ormazabal <patricio.lyono@gmail.com>
2024-01-16 12:34:04 -0300

    Header y Footer
    Header y Footer completos listo para seguir con el contenido

---
+++ HlorDigital/resources/views/includes/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="mt-auto py-3 bg-light" style="width:100%; position:absolute; bottom:0">
+  <div class="container">
+    <span class="text-muted">© 2024 Desarrollado con <span style="color:red">❤</span> por Departamento de TI Hospital Leopoldo Ortega</span>
+  </div>
+</footer>
